ec22399d4d9f00745f38a17206453e379d1f5d6b
Jiyong Ha <slave5vw@gmail.com>
2019-07-17 18:53:17 +0900

    changed fbft message to serialization for tcp  + Simplify fbft message types

--- node/friday_fbft_validator.go
+++ node/friday_fbft_validator.go
@@ -15,36 +15,43 @@ func (f *fridayFBFT) prepareValidatorPhase(b block.Block) error {
 		return errors.New("Failed block bls signing")
 	}
 
-	toSendPrepareMessage := fbft.PrepareMessage{
-		Sign:      *blockSign,
-		PublicKey: f.node.blsPublicKey,
+	toSendPrepareMessage := fbft.Message{
+		Sign:   *blockSign,
+		Pubkey: *f.node.blsSecretKey.GetPublicKey(),
 	}
-	prepareTx := signature.New(f.node.id, signature.Prepare, b.Header.Height, toSendPrepareMessage)
-	f.node.peer.sendSignature(prepareTx)
+	serializedMessage := toSendPrepareMessage.Serialize()
+
+	prepareTx := signature.New(f.node.id, signature.Prepare, b.Header.Height, serializedMessage)
+	f.node.channel.sendSignature(prepareTx)
 	f.node.logger.Info("Send prepare messsage", "blockheight", b.Header.Height)
 
 	return nil
 }
 
-func (f *fridayFBFT) onPreparedValidatorPhase(b block.Block) (fbft.PreparedMessage, error) {
+func (f *fridayFBFT) onPreparedValidatorPhase(b block.Block) (fbft.Message, error) {
 	//OnPrepared Phase - wait leader bls-aggregated message
 	receivedTx := f.node.pool.waitAndRemove(signature.Prepared, b.Header.Height, 1)
 	if len(receivedTx) != 1 {
-		return fbft.PreparedMessage{}, errors.New("Cannot received leader prepared message")
+		return fbft.Message{}, errors.New("Cannot received leader prepared message")
 	}
 
 	// TODO:: Add more leader message validate condition
 	// - check between known leader public key to received leader public key
 	// - check match between received public keys to known validator public keys
-	preparedMessage := receivedTx[0].Payload.(fbft.PreparedMessage)
-	if !preparedMessage.AggregatedSign.VerifyHash(&preparedMessage.AggregatedPubKey, b.Hash[:]) {
-		return fbft.PreparedMessage{}, errors.New("Invalid Leader prepared message")
+	var deserializedMessage fbft.Message
+	err := deserializedMessage.Deserialize(receivedTx[0].Payload.([]byte))
+	if err != nil {
+		return fbft.Message{}, err
+	}
+
+	if !deserializedMessage.Sign.VerifyHash(&deserializedMessage.Pubkey, b.Hash[:]) {
+		return fbft.Message{}, errors.New("Invalid Leader prepared message")
 	}
 	f.node.logger.Info("Received prepared leader message", "blockheight", b.Header.Height)
-	return preparedMessage, nil
+	return deserializedMessage, nil
 }
 
-func (f *fridayFBFT) finalizeValidatorPhase(b block.Block, preparedMessage fbft.PreparedMessage) error {
+func (f *fridayFBFT) finalizeValidatorPhase(b block.Block, preparedMessage fbft.Message) error {
 	//Commit Phase - send commit message
 	messageHash := preparedMessage.Hash()
 	messageSign := f.node.blsSecretKey.SignHash(messageHash[:])
@@ -52,18 +59,18 @@ func (f *fridayFBFT) finalizeValidatorPhase(b block.Block, preparedMessage fbft.
 		return errors.New("failed message bls signing")
 	}
 
-	toSendMessage := fbft.CommitMessage{
-		Sign:      *messageSign,
-		PublicKey: f.node.blsPublicKey,
+	toSendMessage := fbft.Message{
+		Sign:   *messageSign,
+		Pubkey: *f.node.blsSecretKey.GetPublicKey(),
 	}
-	commitTx := signature.New(f.node.id, signature.Commit, b.Header.Height, toSendMessage)
-	f.node.peer.sendSignature(commitTx)
+	commitTx := signature.New(f.node.id, signature.Commit, b.Header.Height, toSendMessage.Serialize())
+	f.node.channel.sendSignature(commitTx)
 	f.node.logger.Info("send prepare messsage", "blockheight", b.Header.Height)
 
 	return nil
 }
 
-func (f *fridayFBFT) onFinalizedValidatorPhase(b block.Block, preparedMessage fbft.PreparedMessage) ([]signature.Signature, error) {
+func (f *fridayFBFT) onFinalizedValidatorPhase(b block.Block, preparedMessage fbft.Message) ([]signature.Signature, error) {
 	//OnCommited Phase -  Wait leader bls-aggregated message
 	receivedTx := f.node.pool.waitAndRemove(signature.Commited, b.Header.Height, 1)
 	if len(receivedTx) != 1 {
@@ -74,8 +81,13 @@ func (f *fridayFBFT) onFinalizedValidatorPhase(b block.Block, preparedMessage fb
 	// - check between known leader public key to received leader public key
 	// - check match between received public keys to known validator public keys
 	messageHash := preparedMessage.Hash()
-	commitedMessage := receivedTx[0].Payload.(fbft.CommitedMessage)
-	if !commitedMessage.AggregatedSign.VerifyHash(&commitedMessage.AggregatedPubKey, messageHash[:]) {
+	var deserializedMessage fbft.Message
+	err := deserializedMessage.Deserialize(receivedTx[0].Payload.([]byte))
+	if err != nil {
+		return []signature.Signature{}, err
+	}
+
+	if !deserializedMessage.Sign.VerifyHash(&deserializedMessage.Pubkey, messageHash[:]) {
 		return []signature.Signature{}, errors.New("Invalid aggregated-bls on leader commited message")
 	}
 	f.node.logger.Info("Received commited leader message", "blockheight", b.Header.Height)
